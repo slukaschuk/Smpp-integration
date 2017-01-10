b43ad10df473f69eaa6313f3894de2ba6abcdb65
Sergey Lukaschuk <sergey.lukaschuk@gmail.com>
2017-01-10 21:07:45 +0200

    Create project

---
+++ src/test/java/io/smsc/integration/SmppApplicationTests.java
@@ -0,0 +1,16 @@
+package io.smsc.integration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
