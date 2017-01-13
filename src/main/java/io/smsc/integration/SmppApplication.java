0d413cff3ba0f04cfa70827e58d39180725f5a80
Sergey Lukaschuk <sergey.lukaschuk@gmail.com>
2017-01-13 09:01:06 +0200

    add smpp library

--- src/main/java/io/smsc/integration/SmppApplication.java
+++ src/main/java/io/smsc/integration/SmppApplication.java
@@ -1,12 +1,26 @@
 package io.smsc.integration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ImportResource;
+
+import java.io.IOException;
 
 @SpringBootApplication
+@ImportResource("/META-INF/integration.xml")
 public class SmppApplication {
+	private static Logger log = LoggerFactory.getLogger(SmppApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(SmppApplication.class, args);
+	public static void main(String[] args) throws IOException {
+		ConfigurableApplicationContext ctx = new SpringApplication(SmppApplication.class).run(args);
+		//log.info("started");
+		System.out.println("Hit Enter to terminate");
+		System.in.read();
+		ctx.close();
+
+
 	}
 }
--- src/test/java/io/smsc/integration/SmppApplicationTests.java
+++ src/test/java/io/smsc/integration/SmppApplicationTests.java
@@ -2,15 +2,30 @@ package io.smsc.integration;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.PollableChannel;
+import org.springframework.messaging.support.GenericMessage;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SmppApplicationTests {
 
+	@Autowired
+	PollableChannel inboundChannel;
+
+	@Autowired
+	MessageChannel outboundChannel;
+
 	@Test
 	public void contextLoads() {
+
+
 	}
+
+
 
 }
